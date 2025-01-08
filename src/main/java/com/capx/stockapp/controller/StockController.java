1b6d22ddfcea8de2a5f7be7e0158ceaefc20d106
Sahil Khairnar <95856189+SahilxxD@users.noreply.github.com>
2025-01-08 21:53:29 +0530

    Update StockController.java

--- src/main/java/com/capx/stockapp/controller/StockController.java
+++ src/main/java/com/capx/stockapp/controller/StockController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.capx.stockapp.model.Stock;
 import com.capx.stockapp.model.StockEntity;
